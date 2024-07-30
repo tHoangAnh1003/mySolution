d0d09950ef286cea8bc51a004727c10233dc735f
tHoangAnh1003 <t.hoang.1003.anh@gmail.com>
2024-07-30 09:21:23 +0700

    W5/Bai5 Course UET INT1008_51 BUG

---
+++ CodeDao/BT_W5/Bai5.cpp
@@ -0,0 +1,15 @@
+void trim_left(char *s) {
+    char* left = s;
+    
+    while (*left == ' ') {
+        left ++;
+    }
+    
+    if (left != s) {
+        char *d = s;
+        while (left) {
+            (*d ++) = (*left ++);
+        }
+        *d = '\0';
+    }
+}
