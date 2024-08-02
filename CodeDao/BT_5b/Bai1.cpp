dfd780e059cedaba821aa98fd399a8aca72529b0
tHoangAnh1003 <t.hoang.1003.anh@gmail.com>
2024-08-02 10:13:34 +0700

    w5_b/Bai1 Courses UET INT2215_50

---
+++ CodeDao/BT_5b/Bai1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+// #include <pair>
+using namespace std;
+
+const int maxN = 1000;
+char a[maxN][maxN];
+
+int main() {
+    int n, m; cin >> n >> m;
+    for (int i = 0; i < n; ++i) 
+        for (int j = 0; j < m; ++j)
+            cin >> a[i][j];
+            
+            
+    vector<pair<int, int>> moves = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            int count = 0;
+            if (a[i][j] == '.') {
+                for (auto it : moves) {
+                    int x = i + it.first;
+                    int y = j + it.second;
+                    if (x >= 0 && x < n && y >= 0 && y < m && a[x][y] == '*')
+                        count += 1;
+                }
+                a[i][j] = char(count + 48);
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << a[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
