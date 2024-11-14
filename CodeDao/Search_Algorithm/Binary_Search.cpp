132ad77f447df8df3766adbe137bc7011ba0ffcf
tHoangAnh1003 <t.hoang.1003.anh@gmail.com>
2024-11-14 07:59:57 +0700

    Search Algorithm

---
+++ CodeDao/Search_Algorithm/Binary_Search.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+#define maxN int(1000005)
+
+int arr[maxN];
+
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) {
+            return mid + 1;
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+
+
+int main() {
+    int n; cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int t; cin >> t;
+    for (int i = 0; i < t; ++i) {
+        int target; cin >> target;
+        cout << binarySearch(arr, n, target) << '\n';
+    }
+    return 0;
+}
---
+++ CodeDao/Search_Algorithm/Linear_Search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+#define maxN int(1000005)
+
+int arr[maxN];
+
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == target) {
+            return i + 1;
+        }
+    }
+
+    return -1;
+}
+
+
+int main() {
+    int n; cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int test; cin >> test;
+    for (int i = 0; i < test; ++i) {
+        int target; cin >> target;
+        cout << linearSearch(arr, n, target) << '\n';
+    }
+    return 0;
+}
